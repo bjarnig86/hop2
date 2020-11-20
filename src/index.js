e3c06be32ed84af214dd60679f4ac8f7ceadc98a
bjarnig86 <72604468+bjarnig86@users.noreply.github.com>
2020-11-20 13:38:46 +0000

    index.js byrjun

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { fetchVideo } from './lib/api';
+import { el, element, empty } from './lib/utils';
 
 document.addEventListener('DOMContentLoaded', async () => {
 const data = await fetchVideo();
 console.log(data);
+
+
 });
